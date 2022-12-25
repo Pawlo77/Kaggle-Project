fb25aa1a2ef8e8b5551bd37ee1080eed867964c3
Pawlo77 <pawpoz16@gmail.com>
2022-12-25 23:44:56 +0100

    pre_computed

---
+++ scripts/models/definitions.py
@@ -0,0 +1,101 @@
+import xgboost as xgb
+
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import f1_score, roc_auc_score
+
+RANDOM_STATE = 42
+
+_LOGISTIC = "LogisticRegression"
+_SVC = "SVC"
+_MLP = "MLPClassifier"
+_XGB = "XGBClassifier"
+
+SCORES = [f1_score, roc_auc_score]
+
+PARAMS = {}
+PARAMS[_LOGISTIC] = {
+    "max_iter": 10000,
+    "random_state": RANDOM_STATE,
+    "solver": "liblinear",
+}
+PARAMS[_SVC] = {"random_state": RANDOM_STATE}
+PARAMS[_MLP] = {
+    "random_state": RANDOM_STATE,
+    "max_iter": 10000,
+    "early_stopping": True,
+    "learning_rate": "adaptive",
+    "solver": "lbfgs",
+}
+PARAMS[_XGB] = {
+    "objective": "binary:logistic",
+    "random_state": RANDOM_STATE,
+    "n_jobs": -1,
+    "early_stopping_rounds": 10,
+}
+
+
+def set_objective(trial, X_train, y_train, classifier_name):
+    if classifier_name == _LOGISTIC:
+        params = {
+            "C": trial.suggest_float("C", 1e-10, 1000, log=True),
+        }
+        classifier_obj = LogisticRegression(
+            **params,
+            **PARAMS[_LOGISTIC],
+        )
+    elif classifier_name == _SVC:
+        params = {
+            "C": trial.suggest_float("C", 1e-10, 1000, log=True),
+            "kernel": trial.suggest_categorical(
+                "kernel", ["linear", "poly", "rbf", "sigmoid"]
+            ),
+            "degree": trial.suggest_int("degree", 3, 7),
+            "gamma": trial.suggest_categorical("gamma", ["auto", "scale"]),
+        }
+        classifier_obj = SVC(
+            **params,
+            **PARAMS[_SVC],
+        )
+    elif classifier_name == _MLP:
+        params = {
+            "hidden_layer_sizes": trial.suggest_categorical(
+                "hidden_layer_sizes",
+                [
+                    (100,),
+                    (50,),
+                    (150,),
+                    (200,),
+                    (100, 50),
+                    (200, 50),
+                    (100, 20),
+                ],
+            ),
+            "activation": trial.suggest_categorical(
+                "activation", ["logistic", "tanh", "relu"]
+            ),
+        }
+        classifier_obj = MLPClassifier(
+            **params,
+            **PARAMS[_MLP],
+        )
+    elif classifier_name == _XGB:
+        params = {
+            "booster": trial.suggest_categorical(
+                "booster", ["gbtree", "gblinear", "dart"]
+            ),
+            "grow_policy": trial.suggest_categorical("grow_policy", [0, 1]),
+            "max_depth": trial.suggest_int("max_depth", 3, 30),
+            "n_estimators": trial.suggest_int("n_estimators", 50, 300, step=10),
+        }
+        classifier_obj = xgb.XGBClassifier(
+            **params,
+            **PARAMS[_XGB],
+        )
+
+    score = cross_val_score(
+        classifier_obj, X_train, y_train, n_jobs=-1, cv=3, scoring="f1"
+    )
+    return score.mean()
--- scripts/models/search.py
+++ scripts/models/search.py
@@ -1,32 +1,18 @@
 import sys
 import os
+import optuna
 import xgboost as xgb
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+optuna.logging.set_verbosity(optuna.logging.ERROR)
 
+from functools import partial
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 
-from sklearn.metrics import f1_score, roc_auc_score
-
-from tools import load_data
-
-RANDOM_STATE = 42
-LOGISTIC_PARAMS = {"max_iter": 1000, "random_state": RANDOM_STATE, "n_jobs": -1}
-TREE_PARAMS = {
-    "objective": "binary:logistic",
-    "random_state": RANDOM_STATE,
-    "n_jobs": -1,
-}
-SVC_PARAMS = {"random_state": RANDOM_STATE}
-MLP_PARAMS = {
-    "random_state": RANDOM_STATE,
-    "max_iter": 1000,
-    "early_stopping": True,
-    "learning_rate": "adaptive",
-}
-SCORES = [f1_score, roc_auc_score]
+from .definitions import set_objective, _LOGISTIC, _SVC, _MLP, _XGB, SCORES
+from tools import load_data, save_object
 
 
 def get_scores(y_true, y_pred):
@@ -42,10 +28,9 @@ def print_scores(name, scores_train, scores_test):
         print(f"\t- Scores on {name} set:")
         for score_name, score_val in score.items():
             print(f"\t\t- {score_name} - {score_val:2.4f}")
-    print()
 
 
-def find_model():
+def find_model(study_name="banking"):
     X_train = load_data("X_train_f.csv").to_numpy()
     y_train = load_data("y_train_f.csv").to_numpy().ravel()
     X_test = load_data("X_test_f.csv").to_numpy()
@@ -53,15 +38,33 @@ def find_model():
 
     # list of pais model object, model training score
     models = []
+    study = optuna.create_study(
+        direction="maximize",
+        storage="sqlite:///db.sqlite3",
+        study_name=study_name,
+    )
 
-    for model, params in (
-        (LogisticRegression, LOGISTIC_PARAMS),
-        (xgb.XGBClassifier, TREE_PARAMS),
-        (SVC, SVC_PARAMS),
-        (MLPClassifier, MLP_PARAMS),
-    ):
-        model = model(**params)
+    for classifier_name in [_LOGISTIC, _SVC, _MLP, _XGB]:
+        objective = partial(
+            set_objective,
+            X_train=X_train,
+            y_train=y_train,
+            classifier_name=classifier_name,
+        )
+        study.optimize(objective, n_trials=100, n_jobs=-1)
+
+        if classifier_name == _LOGISTIC:
+            model = LogisticRegression
+        elif classifier_name == _SVC:
+            model = SVC
+        elif classifier_name == _MLP:
+            model = MLPClassifier
+        elif classifier_name == _XGB:
+            model = xgb.XGBClassifier
+
+        model = model(**study.best_params)
         model.fit(X_train, y_train)
+
         y_pred_train = model.predict(X_train)
         y_pred_test = model.predict(X_test)
 
@@ -69,3 +72,6 @@ def find_model():
         scores_test = get_scores(y_test, y_pred_test)
         print_scores(model.__class__, scores_train, scores_test)
         models.append((model, scores_test))
+
+    models.sort(key=lambda x: x[1], reverse=True)
+    save_object(models[0], "final_model.pkl")
